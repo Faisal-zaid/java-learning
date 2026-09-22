6e1a7fc4a6644e3e008275d6bea5a0348626a368
faisal ali zaid <zaidalideveloper@gmail.com>
2026-09-23 00:02:59 +0300

    wrote my first command

---
+++ mainn.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(sting(args[])){
+        age=12;
+        x='i am';
+        y='years old';
+
+        system.out.printIn('i am'+ age + "years old")
+
+    }
+}
