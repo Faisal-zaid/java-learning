e487d504f948621a24d68356e9ea90fea9801b9d
faisal ali zaid <zaidalideveloper@gmail.com>
2026-09-23 00:13:43 +0300

    added semi colon and removed some variables

--- mainn.java
+++ mainn.java
@@ -1,10 +1,10 @@
 public class Main{
     public static void main(String[]args){
         int age=12;
-        x="i am";
-        y="years old";
+        // x="i am";
+        // y="years old";
 
-        System.out.println("i am"+ age + "years old")
+        System.out.println("i am"+ age + "years old");
 
     }
 }
