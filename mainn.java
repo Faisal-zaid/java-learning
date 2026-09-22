01b72af29c85259618e014de7ca280b46f0789b8
faisal ali zaid <zaidalideveloper@gmail.com>
2026-09-23 00:08:27 +0300

    changed from single quotation to double quotation in java string

--- mainn.java
+++ mainn.java
@@ -1,8 +1,8 @@
 public class Main{
     public static void main(String[]args){
         int age=12;
-        x='i am';
-        y='years old';
+        x="i am";
+        y="years old";
 
         system.out.printIn('i am'+ age + "years old")
 
